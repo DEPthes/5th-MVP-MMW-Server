f3cd0d89ce15fa9209a996d0593cc8b64d8b98b7
juzeo <65641506+juzeo@users.noreply.github.com>
2026-08-20 23:49:03 +0900

    feat: CORS 허용 도메인 추가

--- src/main/java/com/wvw/mmw/global/config/WebConfig.java
+++ src/main/java/com/wvw/mmw/global/config/WebConfig.java
@@ -16,7 +16,11 @@ public class WebConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost:5180",
+                "https://moamyeonwan-web.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
